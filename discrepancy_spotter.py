366721ca36170884138fb6286217301807f70a7d
Sean Seung Won Lee <swdev1202@gmail.com>
2019-07-24 12:48:55 -0700

    discrepancy spotter added

--- discrepancy_spotter.py
+++ discrepancy_spotter.py
@@ -14,4 +14,13 @@ for disp in disp_list:
     disp_split = disp.split('.')
     only_time_disp.append(disp_split[0][:10])
 
-print(only_time_disp)
+only_time_left = []
+for left in left_list:
+    left_split = left.split('.')
+    left_split = left_split[0]
+    left_split = left_split.split('_')
+    only_time_left.append(left_split[3][:10])
+
+print(only_time_left)
+print(len(only_time_disp))
+print(len(only_time_left))
