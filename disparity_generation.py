44c88dadc874568cc79dc243717b42a3e6e9bbee
Sean Seung Won Lee <swdev1202@gmail.com>
2019-07-24 23:20:12 -0700

    adding identifier each file

--- disparity_generation.py
+++ disparity_generation.py
@@ -7,9 +7,9 @@ from argoverse.utils.ply_loader import load_ply
 from argoverse.utils.camera_stats import get_image_dims_for_camera as get_dim
 
 root_dir = '/data/cmpe297-03-sp19/PilotA/Argoverse_3d_tracking/argoverse-tracking/'
-disparity_dir = root_dir + 'disparity2/'
-stereo_left_dir = root_dir + 'stereo_left2/'
-stereo_right_dir = root_dir + 'stereo_right2/'
+disparity_dir = root_dir + 'disparity1/'
+stereo_left_dir = root_dir + 'stereo_left1/'
+stereo_right_dir = root_dir + 'stereo_right1/'
 
 # trains = ['train1/', 'train2/', 'train3/', 'train4/']
 
@@ -21,7 +21,7 @@ def match_and_return(outer_loop, inner_loop):
                     return_list.append(inner_loop[j])
     return return_list
 
-subroot_dir = root_dir + 'train2/'
+subroot_dir = root_dir + 'train1/'
 argoverse_loader = ArgoverseTrackingLoader(subroot_dir)
 print('Total number of logs:',len(argoverse_loader))
 argoverse_loader.print_all()
@@ -32,6 +32,7 @@ camR = argoverse_loader.CAMERA_LIST[8] # right stereo
 baseline = 0.3
 
 for log_id in argoverse_loader.log_list:
+    get_log_identifier = log_id.split('-')[0]
     print("processing log:", log_id)
     argoverse_data = argoverse_loader.get(log_id)
     calibL = argoverse_data.get_calibration(camL)
@@ -66,7 +67,6 @@ for log_id in argoverse_loader.log_list:
         for j in range(len(lidar_timeonly)):
             if(lidar_timeonly[j] == stereo_timeonly[i]):
                 overlap.append(stereo_timeonly[i])
-    print("overlapping = " + str(len(overlap)))
 
     # sort only corresponding lidar files (timestamp only)
     only_lidar_time = [] 
@@ -74,34 +74,19 @@ for log_id in argoverse_loader.log_list:
         for j in range(len(lidar_timestamp_list)):
                 if(overlap[i] in str(lidar_timestamp_list[j])):
                     only_lidar_time.append(lidar_timestamp_list[j])
-    print("only_lidar_time = " + str(len(only_lidar_time)))
 
     # sort only corresponding lidar files (absolute file location)
     only_lidar = match_and_return(overlap, lidar_list)
-    print("only_lidar = " + str(len(only_lidar)))
 
     # sort only corresponding stereo left files (absolute file location)
     only_left = match_and_return(overlap, stereo_left_list['stereo_front_left'])
-    print("only_left = " + str(len(only_left)))
                 
     # sort only corresponding stereo right files (absolute file location)
     only_right = match_and_return(overlap, stereo_left_list['stereo_front_right'])
-    print("only_right = " + str(len(only_right)))
-
-    error_file = open('error.txt', 'a')
-    if(len(overlap) != len(only_lidar_time)):
-        error_file.write(log_id)
-    if(len(overlap) != len(only_lidar)):
-        error_file.write(log_id)
-    if(len(overlap) != len(only_left)):
-        error_file.write(log_id)
-    if(len(overlap) != len(only_right)):
-        error_file.write(log_id)
-    error_file.close()
                 
     for idx in range(len(only_lidar)):
         lidar_timestamp = only_lidar_time[idx]
-        # print("index: ", idx, "current timestamp: ", lidar_timestamp)
+        print("index: ", idx, "current timestamp: ", lidar_timestamp)
         
         pc = load_ply(only_lidar[idx])
         
@@ -125,12 +110,19 @@ for log_id in argoverse_loader.log_list:
         disp_map = (calibL.K[0,0] * baseline) / depth_map
         # making all negative values of disparity to -1.0
         disp_map[disp_map < 0] = -1.0
-        np.save(disparity_dir + str(lidar_timestamp), disp_map)  
+        disp_store_name = get_log_identifier + '_' + str(lidar_timestamp)
+        np.save(disparity_dir + disp_store_name, disp_map)
         
     #copy corresponding left and right images to dedicated location
     for i in range(len(overlap)):
-        shutil.copy2(only_left[i], stereo_left_dir)
-        shutil.copy2(only_right[i], stereo_right_dir)
+        get_left_name = only_left[i].split('/')[-1]
+        get_right_name = only_right[i].split('/')[-1]
+
+        left_store_name = get_log_identifier + '_' + get_left_name
+        right_store_name = get_log_identifier + '_' + get_right_name
+
+        shutil.copy2(only_left[i], stereo_left_dir + left_store_name)
+        shutil.copy2(only_right[i], stereo_right_dir + right_store_name)
 
 ''' 
 for train in trains:
